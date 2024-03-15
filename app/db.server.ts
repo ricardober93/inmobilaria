94f49465d29f183842afcd727bc26aa37a706f87
Ricardo Bermudez <ricardo.bermudez@mercado.libre.com>
2024-03-15 18:45:04 -0500

    Actualizar configuración de conexión a la base de datos y eliminar características de vista previa

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,18 +1,9 @@
-import { createClient } from "@libsql/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
 import { singleton } from "./singleton.server";
 
-const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-});
-
-const adapter = new PrismaLibSQL(libsql);
-
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-const prisma = singleton("prisma", () => new PrismaClient({ adapter }));
+const prisma = singleton("prisma", () => new PrismaClient({}));
 prisma.$connect();
 
 export { prisma };
